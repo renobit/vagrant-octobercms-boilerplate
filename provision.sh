3d20f90d5063249e6cf25263522dabef0b411c71
drmyersii <drmyersii@users.noreply.github.com>
2015-07-12 15:23:58 -0400

    Added Nginx setup...

--- provision.sh
+++ provision.sh
@@ -28,3 +28,54 @@ apt-get install -qq mysql-server
 
 # update root password
 mysqladmin -u root password ${mysql_root_password}
+
+
+printf "Installing Nginx..."
+# adding source list
+echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/nginx-stable.list
+
+# adding key
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+
+# install nginx
+apt-get update -qq > /dev/null
+apt-get install -qq nginx
+
+# configure nginx
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
+
+tee /etc/nginx/sites-available/default << 'EOF'
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /vagrant/www;
+        index index.php index.html index.htm;
+
+        server_name _;
+
+        location / {
+                try_files $uri $uri/ /index.html;
+        }
+
+        error_page 404 /404.html;
+        error_page 500 502 503 504 /50x.html;
+
+        location = /50x.html {
+              root /usr/share/nginx/www;
+        }
+
+        # pass the PHP scripts to FastCGI server listening on the php-fpm socket
+        location ~ \.php$ {
+                try_files $uri =404;
+                fastcgi_pass unix:/var/run/php5-fpm.sock;
+                fastcgi_index index.php;
+                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+                include fastcgi_params;
+        }
+
+}
+EOF
+
+# start nginx
+service nginx restart
